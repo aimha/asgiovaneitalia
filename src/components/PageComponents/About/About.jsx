f09f248d3ad1a48cd1a68d73affe0a23e1c651fb
Christian Boragine <christian.boragine@gmail.com>
2025-08-17 18:55:16 +0200

    about layout

--- src/components/PageComponents/About/About.jsx
+++ src/components/PageComponents/About/About.jsx
@@ -22,15 +22,17 @@ function About(props) {
             { aboutDB.title }
           </h2>
           <div className={`${styles.Paragraph}`}>
-            <h3 className={`${styles.SubTitle}`}>
-              { aboutDB.subtitle }
-            </h3>
-            <For each={ aboutDB.body }>
-                {(item, index) =>
-                <p innerHTML={ item } className={`${styles.Body}`}>
-                </p>
-                }
-            </For>
+            <div>
+              <h3 className={`${styles.SubTitle}`}>
+                { aboutDB.subtitle }
+              </h3>
+              <For each={ aboutDB.body }>
+                  {(item, index) =>
+                  <p innerHTML={ item } className={`${styles.Body}`}>
+                  </p>
+                  }
+              </For>
+            </div>
           </div>
         </div>
       </div>
