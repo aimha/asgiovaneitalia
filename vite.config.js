73645280a94d13733967c7a1cd5231d4d11b6dd4
christian.boragine <christian.boragine@sparkfabrik.com>
2025-08-20 19:16:59 +0200

    merge from feature/vw-conversion-refactoring

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   publicDir: './static/',
