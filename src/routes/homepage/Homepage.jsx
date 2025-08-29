995137f1a3bd845ca513b4ac08d74912d24c44d3
christian.boragine <christian.boragine@sparkfabrik.com>
2025-08-29 12:33:04 +0200

    membership smartphone breakpoint, various improvements

--- src/routes/homepage/Homepage.jsx
+++ src/routes/homepage/Homepage.jsx
@@ -38,9 +38,9 @@ function App() {
       {/* PAGE CONTENT */}
       {/* <Hero db={ state.hero }/> */}
       {/* <About db={ state.about }/> */}
-      <History db={ state.history }/>
+      {/* <History db={ state.history }/> */}
       {/* <Activities db={ state.activities }/> */}
-      {/* <Membership db={ state.membership }/> */}
+      <Membership db={ state.membership }/>
       {/* <Where db={ state.where }/>  */}
     </>
   );
