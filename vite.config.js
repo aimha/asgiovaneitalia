3560fe3d4970f3d65796bbcc6582fe96e7f9c45e
christian.boragine <christian.boragine@sparkfabrik.com>
2025-10-01 15:51:05 +0200

    .env variables for deployment added

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,21 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig, loadEnv } from "vite";
+import solid from "vite-plugin-solid";
 
-export default defineConfig({
-  publicDir: './static/',
-  base: "/asgiovaneitalia/",
-  plugins: [
-    solidPlugin()
-  ],
-  server: {
-    port: 3000,
-  },
-  build: {
-    target: 'esnext',
-    outDir: './dist', 
-    emptyOutDir: true,
-    sourcemap: true
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    publicDir: './static/',
+    base: env.VITE_ROUTER_BASE,
+    plugins: [solid()],
+    server: {
+      port: 3000,
+    },
+    build: {
+      target: 'esnext',
+      outDir: './dist', 
+      emptyOutDir: true,
+      sourcemap: true
+    },
+  };
 });
