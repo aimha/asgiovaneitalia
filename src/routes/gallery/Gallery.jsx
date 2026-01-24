25ca4b62bf4f1617a3d3cf9ed0f8ed5f193b88bc
Christian Boragine <christian.boragine@gmail.com>
2026-01-24 20:08:23 +0100

    gallry page stub

--- src/routes/gallery/Gallery.jsx
+++ src/routes/gallery/Gallery.jsx
@@ -11,18 +11,32 @@ import stateManagement from "../../data/stores/Store";
 
 function Gallery() {
   const { state } = stateManagement;
+  let root;
 
   onMount(() => {
     // initialize gallery logic
-    const gllry = new GalleryClass();
+    const gllry = new GalleryClass(root, styles);
     gllry.init();
   });
 
   return (
-	<>
-    <div>
-      Hello from Gallery Page!
-    </div>
+    <>
+      <div ref={root}>
+        Hello from Gallery Page!
+        <section class={`${styles.GalleryContainer} section slide`}>
+          <ul class={`${styles.Gallery}`}>
+            <For each={state.gallery}>
+              {(item, index) =>
+                <li class={`${styles.GalleryEl}`}>
+                  <div class={`${styles.Overlay}`}></div>
+                  <img src={item.thumbnail_url} alt="" />
+                  <h3>{item.title}</h3>
+                </li>
+              }
+            </For>
+          </ul>
+        </section>
+      </div>
     </>
   )
 }
--- src/routes/gallery/Gallery.module.js
+++ src/routes/gallery/Gallery.module.js
@@ -1,9 +1,11 @@
 // JS CLASS
 
 export default class GalleryClass {
-	constructor() {
+  constructor(_root, _styles) {
+    this.root = _root;
+    this.styles = _styles;
+  }
 
-	}
   init() {
     console.log("Gallery class working!")
   }
